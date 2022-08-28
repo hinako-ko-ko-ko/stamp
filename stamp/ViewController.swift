0c86160b7550886e2db2b26425bb27bbcde4bc5a
Hinako Nakai <hinako.0113n@gmail.com>
2022-08-29 05:02:06 +0900

    Initial Commit

---
+++ stamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stamp
+//
+//  Created by 中井日向子 on 2022/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
